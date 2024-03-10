7590762cb19c85e42ddc4af7812ff8935cbb8dfd
Louis Johnson <louiszn269@gmail.com>
2024-03-10 23:19:35 +0700

    feat: export

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { default as REST } from "./rest/REST.js";
+
+export { default as Client } from "./Client.js";
+
+export { default as Shard } from "./sharding/Shard.js";
+export { default as ShardManager } from "./sharding/ShardManager.js";
