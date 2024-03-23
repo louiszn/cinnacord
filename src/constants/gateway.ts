65ce1956111f3b099ba094bb9af2c3da6e4343f1
Louis Johnson <louiszn269@gmail.com>
2024-03-23 22:33:05 +0700

    feat: added gateway intents

--- src/constants/gateway.ts
+++ src/constants/gateway.ts
@@ -1,3 +1,25 @@
+export enum GatewayIntents {
+	Guilds = 1 << 0,
+	GuildMembers = 1 << 1,
+	GuildModeration = 1 << 2,
+	GuildEmojisAndStickers = 1 << 3,
+	GuildIntegrations = 1 << 4,
+	GuildWebhooks = 1 << 5,
+	GuildInvites = 1 << 6,
+	GuildVoiceStates = 1 << 7,
+	GuildPresences = 1 << 8,
+	GuildMessages = 1 << 9,
+	GuildMessageReactions = 1 << 10,
+	GuildMessageTyping = 1 << 11,
+	DirectMessages = 1 << 12,
+	DirectMessageReactions = 1 << 13,
+	DirectMessageTyping = 1 << 14,
+	MessageContent = 1 << 15,
+	GuildScheduledEvents = 1 << 16,
+	AutoModerationConfiguration = 1 << 20,
+	AutoModerationExecution = 1 << 21,
+}
+
 /**
  * https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway-gateway-opcodes
  */
@@ -24,6 +46,7 @@ export enum CloseEventCodes {
 	DecodeError,
 	NotAuthenticated,
 	AuthenticationFailed,
+	AlreadyAuthenticated,
 	InvalidSeq = 4007,
 	RateLimited,
 	SessionTimedOut,
