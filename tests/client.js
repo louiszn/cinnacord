c7c1b34d1e6dba57f56d4a807f208101c157d201
Louis Johnson <louiszn269@gmail.com>
2024-03-23 22:35:41 +0700

    chore: test file

--- tests/client.js
+++ tests/client.js
@@ -1,14 +1,49 @@
-import { Client } from "../dist/index.js";
+import { Client, GatewayIntents } from "../dist/index.js";
+
+const config = {
+	token: "",
+};
 
 const client = new Client({
-	intents: 1 << 0,
-	token: "A_VERY_REAL_TOKEN",
+	intents: GatewayIntents.Guilds | GatewayIntents.GuildMessages | GatewayIntents.MessageContent,
+	token: config.token,
 });
 
 client.on("debug", console.log);
 
-// client.on("ready", () => {
-// 	console.log("Client is ready!");
-// });
+client.on("raw", async ({ data, name }) => {
+	if (name === "MESSAGE_CREATE") {
+		if (data.author.bot) {
+			return;
+		}
 
-client.connect();
+		if (data.content === "!ping") {
+			await client.rest.post(`/channels/${data.channel_id}/messages`, {
+				content: `Pong! ${client.latency}ms!`,
+			});
+		}
+
+		if (data.content === "!avatar") {
+			const embed = {
+				author: { name: data.author.username },
+				image: {
+					url: `https://cdn.discordapp.com/avatars/${data.author.id}/${data.author.avatar}.png?size=4096`,
+				},
+			};
+
+			await client.rest.post(`/channels/${data.channel_id}/messages`, {
+				embeds: [embed],
+			});
+		}
+
+		if (data.content === "!disconnect") {
+			client.shards.get(0).disconnect();
+
+			await client.rest.post(`/channels/${data.channel_id}/messages`, {
+				content: "Websocket disconnected.",
+			});
+		}
+	}
+});
+
+await client.connect();
