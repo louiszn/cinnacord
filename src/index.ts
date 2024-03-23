b6f8b74e5ead134bbfa69b18bf743585c0c52f9d
Louis Johnson <louiszn269@gmail.com>
2024-03-23 22:32:40 +0700

    chore: updated the exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+export * from "./constants/client.js";
+export * from "./constants/gateway.js";
+
 export * from "./rest/REST.js";
+export * from "./rest/Bucket.js";
 
 export * from "./Client.js";
 
 export * from "./sharding/Shard.js";
 export * from "./sharding/ShardManager.js";
+
