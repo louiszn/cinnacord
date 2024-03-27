f7fbfb7d2221add22725016ecfaff66a23acad43
Louis Johnson <louiszn269@gmail.com>
2024-03-27 23:30:22 +0700

    chore: ChannelTypes

---
+++ src/constants/channel.ts
@@ -0,0 +1,15 @@
+export enum ChannelTypes {
+	GuildText,
+	DM,
+	GuildVoice,
+	GroupDM,
+	GuildCategory,
+	GuildAnnouncement,
+	AnnouncementThread = 10,
+	PublicThread,
+	PrivateThread,
+	GuildStageVoice,
+	GuildDirectory,
+	GuildForum,
+	GuildMedia,
+}
